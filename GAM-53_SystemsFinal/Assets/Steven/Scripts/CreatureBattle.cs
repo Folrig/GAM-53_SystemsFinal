902341276cb8fd9c94cabf58cf071561d9f46d66
Steven Cheney <scheney@student.rccd.edu>
2019-05-08 14:38:09 -0700

    Added some directory structure, a skeleton CreatureBattle script, and an example unit test script

---
+++ GAM-53_SystemsFinal/Assets/Steven/Scripts/CreatureBattle.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CreatureBattle : MonoBehaviour 
+{
+    public BattleCreature playerCreature;
+    private BattleCreature enemyCreature;
+
+    public IEnumerator DoAttack(BattleMove attack)
+    {
+        yield return null;
+    }
+
+    private void ShowMessage(string message)
+    {
+        
+    }
+
+    private IEnumerator DoEnemyTurn()
+    {
+        yield return null;
+    }
+
+    private void FinishBattle()
+    {
+        
+    }
+
+    private BattleCreature GenerateEnemy(int level)
+    {
+        return null;
+    }
+}
---
+++ GAM-53_SystemsFinal/Assets/Steven/Scripts/Editor/CreatureBattle_Test.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+public class CreatureBattle_Test 
+{
+    [Test]
+    public void PassingTestTester()
+    {
+        Assert.That(true);
+    }
+
+    [Test]
+    public void FailingTestTester()
+    {
+        Assert.That(false);
+    }
+}
