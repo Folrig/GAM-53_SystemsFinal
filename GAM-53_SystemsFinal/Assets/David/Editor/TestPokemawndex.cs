ae2a0206dbe290c2a0c530b404d09eb3a7e91269
Delmin100 <48076095+Delmin100@users.noreply.github.com>
2019-05-08 15:41:58 -0700

    just pushing in what i started David

---
+++ GAM-53_SystemsFinal/Assets/David/Editor/TestPokemawndex.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+
+
+public class TestPokemawndex
+{
+    private List<CreatureBattle> Battle;
+    private string creatureName;
+    private int skill;
+
+
+	void Start ()
+    {
+		
+	}
+	
+	
+	void Update ()
+    {
+		
+	}
+
+    public void Creature(string newName, int newSkill)
+    {
+        creatureName = newName;
+        skill = newSkill;
+    }
+}
