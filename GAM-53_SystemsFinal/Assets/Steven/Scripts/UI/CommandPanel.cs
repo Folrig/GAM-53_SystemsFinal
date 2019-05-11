ff8f819ad0e684f85afe63963c738f43e7b68727
Steven Cheney <scheney@student.rccd.edu>
2019-05-10 17:27:19 -0700

    [CreatureBattle] Repurposed a script folder into a broader UI script folder. Added a script to manage the command panel. MessageList now properly instantiates GameObjects with Text components on them, not just Text components.

---
+++ GAM-53_SystemsFinal/Assets/Steven/Scripts/UI/CommandPanel.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CommandPanel : MonoBehaviour
+{
+    [SerializeField] private GameObject commandPrefab;
+    [SerializeField] private Image panel;
+
+    private bool hidden = false;
+    private List<GameObject> commands = new List<GameObject>();
+
+    public void AddCommand(string command)
+    {
+        GameObject newCommand = Instantiate(commandPrefab, transform);
+        Button commandButton = newCommand.GetComponent<Button>();
+        if (commandButton == null)
+        {
+            Debug.LogError("[F" + Time.frameCount + "]: Command prefab does not have a Button component!");
+            Destroy(newCommand);
+            return;
+        }
+
+        Text commandText = newCommand.GetComponentInChildren<Text>();
+        if (commandText == null)
+        {
+            Debug.LogWarning("[F" + Time.frameCount + "]: Command prefab does not have button text.");
+        }
+        else
+        {
+            commandText.text = command;
+        }
+
+        commandButton.onClick.AddListener(() => CommandClicked(command));
+        commands.Add(newCommand);
+    }
+
+    public void RemoveAllCommands()
+    {
+        foreach (GameObject command in commands)
+        {
+            commands.Remove(command);
+            Destroy(command);            
+        }
+    }
+
+    public void TogglePanel()
+    {
+        TogglePanel(hidden);
+    }
+
+    public void TogglePanel(bool active)
+    {
+        panel.enabled = active;
+        foreach (GameObject command in commands)
+        {
+            command.SetActive(active);
+        }
+        hidden = !active;
+    }
+
+    // Called when a command button is clicked
+    public void CommandClicked(string command)
+    {
+        Debug.Log("[F" + Time.frameCount + "]: Command \"" + command + "\" clicked.");
+    }
+}
---
+++ GAM-53_SystemsFinal/Assets/Steven/Scripts/UI/MessageList.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MessageList : MonoBehaviour 
+{
+    [SerializeField] private VerticalLayoutGroup layout;
+    [SerializeField] private Scrollbar scrollbar;
+    [SerializeField] private GameObject messagePrefab;
+
+    const float SIZE_PER_LINE = 22f;
+    const float PADDING_PER_LINE = 1f;
+
+    private float verticalHeight = 0.0f;
+
+    public void AddMessage(string message)
+    {
+        GameObject newMessage = Instantiate(messagePrefab, transform);
+        Text messageText = newMessage.GetComponent<Text>();
+        if (messageText == null)
+        {
+            Debug.LogError("[F" + Time.frameCount + "]: Message prefab does not have a Text component!");
+            Destroy(newMessage);
+            return;
+        }
+        messageText.text = message;
+        // Get fresh text line information
+        Canvas.ForceUpdateCanvases();
+
+        // Fit text to content since VLGs don't cooperate with Content Fitters
+        int lines = messageText.cachedTextGenerator.lineCount;
+        float newTextHeight = lines * SIZE_PER_LINE + (lines - 1) * PADDING_PER_LINE;
+        RectTransform textRect = newMessage.transform as RectTransform;
+        textRect.sizeDelta = new Vector2(textRect.sizeDelta.x, newTextHeight);
+
+        // Expand list to accomodate new message
+        RectTransform listRect = transform as RectTransform;
+        listRect.sizeDelta = new Vector2(listRect.sizeDelta.x, listRect.sizeDelta.y + newTextHeight + layout.spacing);
+
+        scrollbar.value = 1f;
+    }
+
+	private void OnEnable()
+	{
+		if (layout == null ||
+            scrollbar == null ||
+            messagePrefab == null)
+        {
+            Debug.LogError("[F" + Time.frameCount + "]: Something isn't hooked up to MessageList properly. Disabling.");
+            gameObject.SetActive(false);
+        }
+	}
+}
