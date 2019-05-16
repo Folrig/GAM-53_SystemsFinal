98bab7b50815e0b17d909af51597e030d22bc6f5
David Chaney <dchaneypin@student.rccd.edu>
2019-05-15 21:05:28 -0700

    things i added before

---
+++ GAM-53_SystemsFinal/Assets/David/Scripts/Using Monobehaviour Scripts/ButtonManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using NUnit.Framework;
+using System;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+//[TestFixture]
+//[UnityTestAttribute]
+public class ButtonManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject startButton;
+    [SerializeField]
+    private GameObject savingButton;
+    [SerializeField]
+    private GameObject loadingDataButton;
+    [SerializeField]
+    private GameObject deleteButton;
+    [SerializeField]
+    private GameObject createButton;
+    [SerializeField]
+    private List<PlayerPrefs> BattleCreatures;
+    [SerializeField]
+    private GameObject [] deletes;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Delete()
+    {
+        
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene("DavidTestingScene");
+    }
+    public void SaveGame()
+    {
+
+    }
+    public void CreateNew()
+    {
+
+    }
+    public void LoadData()
+    {
+
+    }
+
+
+}
