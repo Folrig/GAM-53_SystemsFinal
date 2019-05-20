5413ab1c67d6b1b265c47f9a99c9432833f25558
David Chaney <dchaneypin@student.rccd.edu>
2019-05-20 15:31:55 -0700

    pokemawndex ui in class

--- GAM-53_SystemsFinal/Assets/David/Scripts/Using Monobehaviour Scripts/ButtonManager.cs
+++ GAM-53_SystemsFinal/Assets/David/Scripts/Using Monobehaviour Scripts/ButtonManager.cs
@@ -11,6 +11,31 @@ using UnityEngine.SceneManagement;
 public class ButtonManager : MonoBehaviour
 {
     [SerializeField]
+    private Text nameOfCreature;
+    [SerializeField]
+    private Text healthOfCreature;
+
+    [SerializeField]
+    private Button firePower;
+    [SerializeField]
+    private Button waterPower;
+    [SerializeField]
+    private Button earthPower;
+    [SerializeField]
+    private Button goBack;
+
+    [SerializeField]
+    private GameObject listOfCreatures;
+
+    [SerializeField]
+    private Text powerText;
+
+    [SerializeField]
+    private GameObject mainMenu;
+
+    [SerializeField]
+    private GameObject creatingCreaturePanel;
+    [SerializeField]
     private GameObject startButton;
     [SerializeField]
     private GameObject savingButton;
@@ -22,22 +47,24 @@ public class ButtonManager : MonoBehaviour
     private GameObject createButton;
     [SerializeField]
     private List<PlayerPrefs> BattleCreatures;
-    [SerializeField]
-    private GameObject [] deletes;
+    //[SerializeField]
+    // private GameObject [] deletes;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-    public void Delete()
+    // Use this for initialization
+    void Start()
     {
-        
+        powerText.text = ("Power: ");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Delete1()
+    {
+
     }
     public void StartGame()
     {
@@ -45,16 +72,45 @@ public class ButtonManager : MonoBehaviour
     }
     public void SaveGame()
     {
-
+        
+    }
+    public void SaveCreature()
+    {
+        
     }
     public void CreateNew()
     {
-
+        creatingCreaturePanel.SetActive(true);
+        mainMenu.SetActive(false);
     }
     public void LoadData()
     {
 
     }
 
+    public void FirePowerUp()
+    {
+        powerText.text = ("Power: " + ("Fire"));
+    }
+    public void WaterPowerUp()
+    {
+        powerText.text = ("Power: " + ("Water"));
+    }
+    public void EarthPowerUp()
+    {
+        powerText.text = ("Power: " + ("Earth"));
+
+    }
+    public void ShowListOfCreatures()
+    {
+        listOfCreatures.SetActive(true);
+        mainMenu.SetActive(false);
+    }
+    public void BackToMenu()
+    {
+        mainMenu.SetActive(true);
+        listOfCreatures.SetActive(false);
+        creatingCreaturePanel.SetActive(false);
+    }
 
 }
