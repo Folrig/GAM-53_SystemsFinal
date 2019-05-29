a1e06535e455c2135697183ee0c8117a319da93f
David Chaney <dchaneypin@student.rccd.edu>
2019-05-29 16:52:38 -0700

    things added from class

---
+++ GAM-53_SystemsFinal/Assets/David/Scene2 Stuff/PokeIndexScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PokeIndexScript : MonoBehaviour
+{
+    public string[] CreatureIndex;
+    [SerializeField]
+    private int creatureNum = 0;
+    public GameObject ownCreatureData;
+    public List<BattleCreature> creature;
+    void Start()
+    {
+        List<BattleCreature> creature = new List<BattleCreature>();
+        CreatureIndex = new creature;
+        CreatureIndex = new List<BattleCreature>();
+
+        creature.Add(new BattleCreature("Creature1", water, 100, 100, 50, 50));
+        creature.Add(new BattleCreature("Creature2", fire, 80, 80, 45, 65));
+
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < CreatureIndex.Length; i++)
+        {
+            if ( i == creatureNum)
+            {
+               // Debug.Log(CreatureIndex[i]);
+            }
+        }
+    }
+
+    public void PickingCreature(int index)
+    {
+        creatureNum = index;
+    }
+
+    public void 
+
+
+}
