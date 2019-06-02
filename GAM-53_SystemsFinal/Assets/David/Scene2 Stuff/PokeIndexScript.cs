a617d46670d73967e9ae1e3c1890f21487c54a31
David Chaney <dchaneypin@student.rccd.edu>
2019-06-02 15:46:45 -0700

    creature index in progress

--- GAM-53_SystemsFinal/Assets/David/Scene2 Stuff/PokeIndexScript.cs
+++ GAM-53_SystemsFinal/Assets/David/Scene2 Stuff/PokeIndexScript.cs
@@ -1,36 +1,68 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PokeIndexScript : MonoBehaviour
 {
-    public string[] CreatureIndex;
+    public BattleCreature[] CreatureIndex;
     [SerializeField]
     private int creatureNum = 0;
     public GameObject ownCreatureData;
     public List<BattleCreature> creature;
+    public AddingToGrid gridScript;
+    public GameObject GridView;
+    [SerializeField]
+    private int howMany;
+
+   // public CreatureBattle battleSystem;
+
+
+    [SerializeField]
+    private GameObject spaceHolder;
+
     void Start()
     {
+
+        //CreatureBattle battleSystem = new CreatureBattle();
+
+
+
+
+        gridScript = GridView.GetComponent<AddingToGrid>();
+        howMany = 0;
+
+
         List<BattleCreature> creature = new List<BattleCreature>();
-        // David, I really wish you would fix your code errors before pushing a commit.
-        // Code errors stop anybody from playtesting anything until your errors are fixed.
-        //CreatureIndex = new creature;
-        //CreatureIndex = new List<BattleCreature>();
 
-        //creature.Add(new BattleCreature("Creature1", water, 100, 100, 50, 50));
-        //creature.Add(new BattleCreature("Creature2", fire, 80, 80, 45, 65));
+       /* CreatureIndex = new creature;
+        CreatureIndex = new List<BattleCreature>();
 
+        creature.Add(new BattleCreature("Creature1", water, 100, 100, 50, 50));
+        creature.Add(new BattleCreature("Creature2", fire, 80, 80, 45, 65));
+*/
     }
 
     void Update()
     {
-        for (int i = 0; i < CreatureIndex.Length; i++)
+        // gridScript.CreaturesAddToGrid();
+
+        /* GameObject newObj;
+
+         for (int i = 0; i < CreatureIndex.Length; i++)
+         {
+             if ( i == creatureNum)
+             {
+                 newObj = (GameObject)Instantiate(spaceHolder, transform);
+
+                // Debug.Log(CreatureIndex[i]);
+             }
+         }*/
+
+        for (int i = 0; i < creature.Count; i++)
         {
-            if ( i == creatureNum)
-            {
-               // Debug.Log(CreatureIndex[i]);
-            }
         }
+
     }
 
     public void PickingCreature(int index)
@@ -38,7 +70,38 @@ public class PokeIndexScript : MonoBehaviour
         creatureNum = index;
     }
 
-    //public void 
+    public void AddingNewImage()
+    {
+        gridScript.CreaturesAddToGrid();
+        gridScript.AddANewCreature();
+        NewCreatureData();
+    }
+
+	public void OnMouseUp()
+	{
+        gridScript.AddingIsFalse();
+
+	}
+    public void OnMouseDown()
+    {
+        Debug.Log(spaceHolder.name + creature);
+    }
+
+    public void NewCreatureData()
+    {
+        foreach (BattleCreature creation in CreatureIndex)
+        {
+             howMany += 1;
+
+        }
+
+    }
+
+    public void GoingBackToMenu()
+    {
+        SceneManager.LoadScene("DavidTestingScene");
+    }
+
 
 
 }
